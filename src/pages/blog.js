99df55c879590bedd0f20bf27f4d0dc6572a4186
Pete <pete@fitsense.io>
2019-09-06 15:16:21 +1000

    dsfdfs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -50,6 +50,7 @@ export const pageQuery = graphql`
         node {
           title
           slug
+          author
           publishDate(formatString: "MMMM Do, YYYY")
           tags
           heroImage {
