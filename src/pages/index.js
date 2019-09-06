1f2dae5c48f400a2b59170f97b23872440adcead
Pete <pete@fitsense.io>
2019-09-06 15:00:11 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export const pageQuery = graphql`
         node {
           title
           slug
-        location
+    
           publishDate(formatString: "MMMM Do, YYYY")
           tags
           heroImage {
